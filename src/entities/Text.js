51ad86e112b75e7436b7247d198b8668b7fdff88
Victor Silva <victormarques.ia@gmail.com>
2020-06-29 13:08:48 -0300

    feat(entity-text): Text 'entity' class created

---
+++ src/entities/Text.js
@@ -0,0 +1,12 @@
+class Text {
+  constructor (text, posX, posY, align, color, size) {
+    this.text = text;
+    this.posX = posX;
+    this.posY = posY;
+    this.align = align;
+    this.color = color;
+    this.size = size;
+  }
+}
+
+export default Text;

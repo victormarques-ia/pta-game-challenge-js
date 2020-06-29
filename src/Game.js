17fc433dcf662a8ebd65ba2220151c2325bab452
Victor Silva <victormarques.ia@gmail.com>
2020-06-29 12:46:30 -0300

    feat(game-functions): Created 'canvas'

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,15 @@
 
+// ======== Global variables ======== //
+let canvas;
+let ctx;
 
+// ======== Game start procress ======== //
 export function startGame () {
   
-}
+  // Creating canvas
+  canvas = document.createElement('canvas');
+  ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  document.body.insertBefore(canvas, document.body.childNodes[0]);
+} 

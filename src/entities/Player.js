bc61e43120dc859c6a6e9db564a60d31b652ad04
Victor Silva <victormarques.ia@gmail.com>
2020-06-29 12:52:49 -0300

    feat(entity-player): Player 'entity' class created

---
+++ src/entities/Player.js
@@ -0,0 +1,11 @@
+class Player {
+  constructor (posX, posY, width, height, color) {
+    this.posX = posX;
+    this.posY = posY;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+}
+
+export default Player;

5a6e0f249b13ff339db1ef2e1f0b166e3c4850cc
Victor Silva <victormarques.ia@gmail.com>
2020-06-29 13:12:21 -0300

    feat(entity-text): Created drawBackground function

--- src/entities/Background.js
+++ src/entities/Background.js
@@ -9,6 +9,14 @@ class Background {
     this.path = path;
     this.image = new Image();
   }
+
+  drawBackground (ctx) {
+    this.image.src = this.path;
+    ctx.beginPath();
+    ctx.drawImage(this.image, this.posX, this.posY,this.width, this.height);
+    ctx.drawImage(this.image, this.posX + this.width, this.posY,this.width, this.height);
+    ctx.closePath();
+  }
 }
 
 export default Background;

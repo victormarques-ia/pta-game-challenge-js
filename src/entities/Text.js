e9ce635d5e1b1b159990c1b3c4b41f0b7fc01bf7
Victor Silva <victormarques.ia@gmail.com>
2020-06-29 13:09:11 -0300

    feat(entity-text): Created drawText function

--- src/entities/Text.js
+++ src/entities/Text.js
@@ -7,6 +7,15 @@ class Text {
     this.color = color;
     this.size = size;
   }
+
+  drawText (ctx) {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.font = this.size + "px Notable";
+    ctx.textAlign = this.align;
+    ctx.fillText(this.text, this.posX, this.posY);
+    ctx.closePath();
+  }
 }
 
 export default Text;
